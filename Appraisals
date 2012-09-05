2b54ddf8208b5360c6f24b2712a68cf449d1ed8f
James A. Rosen <james@zendesk.com>
2012-09-04 19:44:18 -0700

    [fix #11] rspec-spies works with rspec 2.11.x
    ArgumentExpectation has been renamed to ArgumentListMatcher. We detect
    which is available and use it.

--- Appraisals
+++ Appraisals
@@ -1,3 +1,7 @@
 appraise "rspec-2-8" do
   gem "rspec", "~> 2.8.0"
 end
+
+appraise "rspec-2-11" do
+  gem "rspec", "~> 2.11.0"
+end
--- lib/rspec-spies.rb
+++ lib/rspec-spies.rb
@@ -17,13 +17,17 @@ end
 
 require 'rspec/expectations'
 require 'rspec/matchers'
+argument_expectation_class = RSpec::Mocks.const_defined?(:ArgumentExpectation) ?
+                               RSpec::Mocks::ArgumentExpectation :
+                               RSpec::Mocks::ArgumentListMatcher
+
 RSpec::Matchers.define :have_received do |method_name, args, block|
   match do |actual|
     messages_received = actual.send(:__mock_proxy).instance_variable_get("@messages_received")
     messages_received.any? do |message|
       received_method_name, received_args, received_block = *message
       result = (received_method_name == method_name)
-      result &&= RSpec::Mocks::ArgumentExpectation.new(@args || any_args).args_match?(received_args)
+      result &&= argument_expectation_class.new(@args || any_args).args_match?(received_args)
       result &&= (received_block == block)
     end
   end
