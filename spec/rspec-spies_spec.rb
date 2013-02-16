8b87f71992b817a5a2d82a5f412d8aeff28563a9
Daniela Wellisz <daniela.wellisz@gmail.com>
2013-02-16 11:47:26 -0800

    fixed bug whereby regexes were not being properly matched

--- spec/rspec-spies_spec.rb
+++ spec/rspec-spies_spec.rb
@@ -37,6 +37,14 @@ module Spec
         have_received(:slice).with(hash_including({ :foo => :baz })).matches?(@object).should be_false
       end
 
+      it "matches if specifies regex, if method is called with matches arguments" do
+        @object.stub!(:slice)
+        @object.slice('foobar')
+
+        have_received(:slice).with(/foo/).matches?(@object).should be_true
+        have_received(:slice).with(/foox/).matches?(@object).should be_false
+      end
+
       it "does not match if method is called with incorrect args" do
         @object.stub!(:slice)
         @object.slice(3)
